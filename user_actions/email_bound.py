e058238d0ed6b76aa323b188f5277f7441dae933
user <yasenovich.mikhail@mail.ru>
2024-04-09 12:39:26 +0300

    added email_bound_request and email_bound_confirm method. Added bound custom email script

---
+++ user_actions/email_bound.py
@@ -0,0 +1,40 @@
+import requests
+from config.config import TestData
+from user_actions.login_user import LoginUsers
+
+login = LoginUsers(username=TestData.USER_NAME, password=TestData.PASSWORD, sec_id=TestData.SEC_ID)
+jwt_auth = login.login_users()
+
+
+def email_bound_request(jwt_token):
+    headers = {
+        'Authorization1': jwt_token
+    }
+
+    body = {
+        'email': TestData.EMAIL
+    }
+
+    response = requests.post(f'{TestData.BASE_URL}' + f'{TestData.POST_EMAIL_REQUEST_PATH}', headers=headers,
+                             json=body)
+    key = response.json()['key']
+    # print(f'Status code: {response.status_code}', key, sep='\n')
+    return key
+
+
+def email_bound_confirm(key):
+    body = {
+        'key': key
+    }
+
+    response = requests.post(f'{TestData.BASE_URL}' + f'{TestData.POST_EMAIL_CONFIRM_PATH}', json=body)
+    print(f'Status code: {response.status_code}', f'Your email has been changed successfully', sep='\n')
+
+
+def bound_email(jwt):
+    key = email_bound_request(jwt)
+    email_bound_confirm(key)
+
+
+bound_email(jwt_auth)
+
