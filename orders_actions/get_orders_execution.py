5201987cc740a252f4b450c6cc9386b1276e433e
user <yasenovich.mikhail@mail.ru>
2024-04-15 16:40:03 +0300

    added get_orders_execution function

---
+++ orders_actions/get_orders_execution.py
@@ -0,0 +1,23 @@
+import requests
+from config.config import TestData
+from user_actions.login_user import LoginUsers
+from pprint import *
+
+
+def get_orders_execution(jwt_token):
+
+    headers = {
+        'accept': 'application/json',
+        'Authorization1': jwt_token
+    }
+
+    response = requests.get(f'{TestData.BASE_URL}' + f'{TestData.GET_ORDERS_EXECUTION_PATH}', headers=headers)
+    print(f'Status code: {response.status_code}')
+    return pprint(response.json())
+
+
+login = LoginUsers(username=TestData.USER_NAME, password=TestData.PASSWORD, sec_id=TestData.SEC_ID)
+jwt = login.login_users()
+get_orders_execution(jwt)
+
+
