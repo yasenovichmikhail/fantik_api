767e7a91af290c596a7652d08c746297965ed200
user <yasenovich.mikhail@mail.ru>
2024-04-23 13:29:09 +0300

    added BaseException class

--- orders_actions/post_create_orders.py
+++ orders_actions/post_create_orders.py
@@ -4,15 +4,21 @@ from user_actions.login_user import LoginUsers
 
 
 def create_orders(jwt_token):
-    headers = {
-        'accept': 'application/json',
-        'Authorization1': jwt_token
-    }
+    try:
+        headers = {
+            'accept': 'application/json',
+            'Authorization1': jwt_token
+        }
 
-    body = TestData.CREATE_ORDER_BODY
+        body = TestData.CREATE_ORDER_BODY
 
-    response = requests.post(f'{TestData.BASE_URL}' + f'{TestData.ORDERS_CREATE_PATH}', headers=headers, json=body)
-    return print(f'Status code: {response.status_code}', f'Your order successfully created', sep='\n')
+        response = requests.post(f'{TestData.BASE_URL}' + f'{TestData.ORDERS_CREATE_PATH}', headers=headers, json=body)
+        if response.status_code == 200:
+            print(f'Your order successfully created', f'Status code: {response.status_code}', sep='\n')
+        else:
+            print(f'Something went wrong, status code: {response.status_code}')
+    except:
+        print(BaseException("Error occurred"))
 
 
 login = LoginUsers(username=TestData.USER_NAME, password=TestData.PASSWORD, sec_id=TestData.SEC_ID)
