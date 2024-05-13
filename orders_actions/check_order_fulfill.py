3f90295e4643c20b71fb57ec818d5f4ce05e8bbe
user <yasenovich.mikhail@mail.ru>
2024-05-13 16:41:15 +0300

    added check_order_fulfill script

--- orders_actions/check_order_fulfill.py
+++ orders_actions/check_order_fulfill.py
@@ -5,6 +5,9 @@ import pandas as pd
 import schedule
 
 conn = TestData.DB_CONNECTION
+AMOUNT_BEFORE = 0
+AMOUNT_AFTER = 0
+FLAG = True
 
 
 def fetch_post_data(aweme_id):
@@ -22,20 +25,21 @@ def fetch_post_data(aweme_id):
         print('Error occurred:\n', traceback.format_exc())
 
 
-def fetch_play_count(post_data):
+def fetch_action_count(post_data, fetch_action):
     try:
         for key, value in post_data.items():
             if key == 'posts':
                 for key_play, value_play in value.items():
-                    if key_play == 'play_count':
+                    if key_play == fetch_action:
                         return value_play
     except BaseExceptions:
         print('Error occurred:\n', traceback.format_exc())
 
 
 def create_message(amount_before, amount_after, order_id):
-    text_message = f'View order {order_id} was successfully completed. Amount before: {amount_before}, ' \
-                   f'amount after: {amount_after}'
+    differance = amount_after - amount_before
+    text_message = f'Your order {order_id} was successfully completed. Amount before: {amount_before}, ' \
+                   f'amount after: {amount_after}. The difference was: {differance}'
     return text_message
 
 
@@ -47,17 +51,26 @@ def send_msg(text):
     print(results.json())
 
 
-def create_view_order():
+def create_order(action_type, aweme_id, amount, fetch_action):
     try:
         login = LoginUsers(username=TestData.USER_NAME, password=TestData.PASSWORD, sec_id=TestData.SEC_ID)
         jwt = login.login_users()
-        create_orders(jwt)
+        create_orders(jwt, action_type, aweme_id, amount)
+        global AMOUNT_BEFORE
+        AMOUNT_BEFORE = fetch_action_count(post_data=fetch_post_data(aweme_id), fetch_action=fetch_action)
     except BaseExceptions:
         print('Error occurred:\n', traceback.format_exc())
 
 
-def fetch_order_id():
-    result = pd.read_sql(TestData.SELECT_ACTIVE_ORDER_ID, conn)
+def fetch_order_id(action_type):
+    select_active_order_id = f"""
+    select order_id
+    from tm_user_orders
+    where user_id = {TestData.USER_ID}
+    and action_type_id = {action_type}
+    order by order_id desc;
+    """
+    result = pd.read_sql(select_active_order_id, conn)
     return result.iloc[0]['order_id']
 
 
@@ -71,19 +84,25 @@ def check_order_status(order_id):
     return order_status_id.iloc[0]['order_status_id']
 
 
-def job():
-    order_id = fetch_order_id()
-    order_status = check_order_status(fetch_order_id())
+def job(action_type_id, aweme_id, fetch_action):
+    order_id = fetch_order_id(action_type_id)
+    order_status = check_order_status(fetch_order_id(action_type_id))
     if order_status == 2:
-        play_count_after = fetch_play_count(fetch_post_data(TestData.AWEME_ID))
-        message = create_message(play_count_before, play_count_after, order_id)
+        global AMOUNT_AFTER
+        AMOUNT_AFTER = fetch_action_count(post_data=fetch_post_data(aweme_id),
+                                          fetch_action=fetch_action)
+        message = create_message(AMOUNT_BEFORE, AMOUNT_AFTER, order_id)
         send_msg(message)
+        global FLAG
+        FLAG = False
 
 
-play_count_before = fetch_play_count(fetch_post_data(TestData.AWEME_ID))
-# create_view_order()
+def create_and_check_order(action_type, aweme_id, amount, fetch_action):
+    create_order(action_type, aweme_id, amount, fetch_action)
+    schedule.every(10).minutes.do(job, action_type, aweme_id, fetch_action)
+    while FLAG:
+        schedule.run_pending()
 
-schedule.every(10).minutes.do(job)
-#
-while True:
-    schedule.run_pending()
+
+create_and_check_order(action_type=TestData.ACTION_TYPE_ID, aweme_id=TestData.AWEME_ID, amount=TestData.ORDER_AMOUNT,
+                       fetch_action=TestData.ACTION_TYPE_VIEWS)
--- orders_actions/post_create_orders.py
+++ orders_actions/post_create_orders.py
@@ -3,25 +3,33 @@ from config.config import *
 from user_actions.login_user import LoginUsers
 
 
-def create_orders(jwt_token):
+def create_orders(jwt_token, action_type, aweme_id, amount):
     try:
         headers = {
             'accept': 'application/json',
             'Authorization1': jwt_token
         }
 
-        body = TestData.CREATE_ORDER_BODY
+        body = {
+            "actionTypeId": action_type,
+            "awemeId": aweme_id,
+            "shortLink": None,
+            "actionsAmount": amount,
+            "orderDurationHours": 0,
+            "orderName": "test",
+            "rewardId": None
+        }
 
         response = requests.post(f'{TestData.BASE_URL}' + f'{TestData.ORDERS_CREATE_PATH}', headers=headers, json=body)
         if response.status_code == 200:
             print(f'Your order successfully created', f'Status code: {response.status_code}', sep='\n')
         else:
             print(f'Something went wrong, status code: {response.status_code}')
-    except:
-        print(BaseException("Error occurred"))
+    except BaseExceptions:
+        print('Error occurred:\n', traceback.format_exc())
 
 
 if __name__ == '__main__':
     login = LoginUsers(username=TestData.USER_NAME, password=TestData.PASSWORD, sec_id=TestData.SEC_ID)
     jwt = login.login_users()
-    create_orders(jwt)
+    create_orders(jwt, 1, TestData.AWEME_ID, 10)
