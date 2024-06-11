34ab042397741321a414b6ba0eb605f71b9aac2a
user <yasenovich.mikhail@mail.ru>
2024-06-11 11:03:28 +0300

    modified parameters of the orders_execution_complete script

--- orders_actions/post_orders_execution_complete.py
+++ orders_actions/post_orders_execution_complete.py
@@ -3,25 +3,33 @@ from config.config import *
 from user_actions.login_user import LoginUsers
 
 
-def post_orders_execution_complete(jwt_token, device_type):
+def post_orders_execution_complete(jwt_token, order_id, device_type=DEVICE_TYPE):
     headers = {
         'accept': 'application/json',
         'Authorization1': jwt_token,
         'device_type_id': device_type
     }
-    body = COMPLETE_ORDERS_BODY
+    body = {
+        "orderId": order_id,
+        "metricsAmountBefore": 1,
+        "metricsAmountAfter": 2,
+        "startDate": CURRENT_TIMESTAMP,
+        "finishDate": CURRENT_TIMESTAMP,
+        "isAvailable": True,
+        "isSkipped": False
+    }
 
     response = requests.post(f'{BASE_URL}' + f'{POST_ORDERS_EXECUTION_COMPLETE_PATH}',
                              headers=headers, json=body)
     print(f'Status code: {response.status_code}')
 
 
-def complete_orders(amount, jwt_token):
+def complete_orders(jwt_token, amount, order_id):
     for i in range(amount):
-        post_orders_execution_complete(jwt_token, DEVICE_TYPE)
+        post_orders_execution_complete(jwt_token=jwt_token, order_id=order_id, device_type=DEVICE_TYPE)
 
 
 login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
 jwt = login.login_users()
 
-complete_orders(ORDER_AMOUNT, jwt)
+complete_orders(jwt, 40, 2893280)
