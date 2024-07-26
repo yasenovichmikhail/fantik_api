6a7db30dd0740c3a4bedc5721005f27f9c50c4d2
user <yasenovich.mikhail@mail.ru>
2024-07-26 16:04:56 +0300

    added choose environment parameter

--- settings_actions/get_settings.py
+++ settings_actions/get_settings.py
@@ -2,18 +2,23 @@
 from config.config import *
 
 
-def get_settings():
+def get_settings(base_url, package_name, device_type):
     try:
-        headers = {'package_name': PACKAGE_NAME,
-                   'device_type_id': DEVICE_TYPE}
+        headers = {'package_name': package_name,
+                   'device_type_id': device_type}
 
-        response = requests.get(f'{BASE_URL}' + f'{GET_SETTINGS_PATH}', headers=headers)
+        response = requests.get(f'{base_url}' + f'{GET_SETTINGS_PATH}', headers=headers)
         print(f'Status code: {response.status_code}')
         assert response.status_code == 200, f"Request is failed with status code: {response.status_code}"
-        return response
+        return pprint(response.json())
     except BaseExceptions:
         print('Error occurred:\n', traceback.format_exc())
 
 
-data = get_settings().json()
-pprint(data)
+def main(base_url, package_name, device_type):
+    get_settings(base_url, package_name, device_type)
+
+
+if __name__ == '__main__':
+    main(BASE_URL_PROD, PACKAGE_NAME_PROD, DEVICE_TYPE)
+
