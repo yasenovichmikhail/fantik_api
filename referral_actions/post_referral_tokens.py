c237eb2b12718f2f5bc6339067cc9aafa90341a2
user <yasenovich.mikhail@mail.ru>
2024-07-26 15:42:02 +0300

    added choose environment parameters to referral program

--- referral_actions/post_referral_tokens.py
+++ referral_actions/post_referral_tokens.py
@@ -4,7 +4,7 @@ from user_actions.login_user import LoginUsers
 from pprint import *
 
 
-def referral_tokens(jwt_token):
+def referral_tokens(jwt_token, base_url):
     try:
         headers = {
             'accept': 'application/json',
@@ -15,7 +15,7 @@ def referral_tokens(jwt_token):
             'usage_type': 'USER_REGISTRATION'
         }
 
-        response = requests.post(f'{BASE_URL}' + f'{REFERRAL_TOKENS_PATH}', headers=headers,
+        response = requests.post(f'{base_url}' + f'{REFERRAL_TOKENS_PATH}', headers=headers,
                                  json=body)
         if response.status_code == 200:
             pprint(response.json())
@@ -25,6 +25,11 @@ def referral_tokens(jwt_token):
         print('Error occurred:\n', traceback.format_exc())
 
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt = login.login_users()
-referral_tokens(jwt)
+def main(base_url):
+    login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
+    jwt = login.login_users(base_url)
+    referral_tokens(jwt, base_url)
+
+
+if __name__ == '__main__':
+    main(BASE_URL_DEV)
--- referral_actions/post_referral_validate.py
+++ referral_actions/post_referral_validate.py
@@ -4,10 +4,8 @@ from user_actions.login_user import LoginUsers
 from pprint import *
 
 
-def referral_validate(jwt_token):
+def referral_validate(jwt_token, base_url):
     try:
-        response_value_expected = ['token', 'token_type', 'validity']
-        response_value_actual = []
         headers = {
             'accept': 'application/json',
             'Authorization1': jwt_token
@@ -15,23 +13,23 @@ def referral_validate(jwt_token):
 
         body = REFERRAL_TOKEN_BODY
 
-        response = requests.post(f'{BASE_URL}' + f'{REFERRAL_VALIDATE_PATH}', headers=headers,
+        response = requests.post(f'{base_url}' + f'{REFERRAL_VALIDATE_PATH}',
+                                 headers=headers,
                                  json=body)
         if response.status_code == 200:
             print(f'Status code: {response.status_code}', sep='\n')
             pprint(response.json())
-            # resp = response.json()
-            # for key in resp.keys():
-            #     response_value_actual.append(key)
-            # assert response_value_actual == response_value_expected, f"Response body doesn't match, " \
-            #                                                          f"Expected {response_value_expected}, " \
-            #                                                          f"got {response_value_actual}"
         else:
             print(f'Something went wrong, status code: {response.status_code}')
     except BaseExceptions:
         print('Error occurred:\n', traceback.format_exc())
 
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt = login.login_users()
-referral_validate(jwt)
+def main(base_url):
+    login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
+    jwt = login.login_users(base_url)
+    referral_validate(jwt, base_url)
+
+
+if __name__ == '__main__':
+    main(BASE_URL_DEV)
