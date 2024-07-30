05c2759cb8140e1886100d21f73dd2b4f12a0a53
user <yasenovich.mikhail@mail.ru>
2024-07-30 10:20:46 +0300

    refactoring of the code

--- orders_actions/create_multiple_different_types_orders.py
+++ orders_actions/create_multiple_different_types_orders.py
@@ -10,14 +10,23 @@ def fetch_random_user_post(username, amount_of_posts):
     return random.choice(all_posts)
 
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt = login.login_users()
-for i in range(1, 5):                                                                                # types of orders
-    for j in range(1):                                                                               # amount of orders
-        if i != 4:
-            create_orders(jwt_token=jwt,
-                          action_type=i,
-                          amount=50,
-                          aweme_id=fetch_random_user_post(USER_NAME, 15))
-        elif i == 4:
-            create_orders(jwt_token=jwt, action_type=i, amount=50)
+def main(base_url, username, password, sec_id, amount):
+    login = LoginUsers(username=username, password=password, sec_id=sec_id)
+    jwt = login.login_users(base_url)
+    for i in range(1, 5):                                                                                # types of orders
+        for j in range(1):                                                                               # amount of orders
+            if i != 4:
+                create_orders(jwt_token=jwt,
+                              action_type=i,
+                              amount=amount,
+                              aweme_id=fetch_random_user_post(username, 15),
+                              base_url=base_url)
+            elif i == 4:
+                create_orders(jwt_token=jwt,
+                              action_type=i,
+                              amount=amount,
+                              base_url=base_url)
+
+
+if __name__ == '__main__':
+    main(base_url=BASE_URL_DEV, username=USER_NAME, password=PASSWORD, sec_id=SEC_ID, amount=10)
