5cbe95a0ebc7f5c8c519057f849c616631f2d4ed
user <yasenovich.mikhail@mail.ru>
2024-07-31 15:53:47 +0300

    added choose scheme parameter

--- deposit_actions/post_deposit.py
+++ deposit_actions/post_deposit.py
@@ -4,26 +4,38 @@ from user_actions.login_user import LoginUsers
 from pprint import *
 
 
-def post_deposits(jwt_token):
+def post_deposits(jwt_token, amount, base_url):
     try:
         headers = {
             'accept': 'application/json',
             'Authorization1': jwt_token
         }
 
-        body = DEPOSIT_BODY
+        body = {
+            'amountDepositsToIncrease': amount
+        }
 
-        response = requests.post(f'{BASE_URL}' + f'{POST_DEPOSITS_PATH}', headers=headers,
+        response = requests.post(f'{base_url}' + f'{POST_DEPOSITS_PATH}', headers=headers,
                                  json=body)
         if response.status_code == 200:
             print(f'Status code: {response.status_code}', sep='\n')
-            pprint(response.json())
+            return response.json()
         else:
             print(f'Something went wrong, status code: {response.status_code}')
-    except:
-        BaseException("Error occurred")
+    except BaseExceptions:
+        print('Error occurred:\n', traceback.format_exc())
 
 
-login = LoginUsers(USER_NAME, PASSWORD, SEC_ID)
-jwt = login.login_users()
-post_deposits(jwt)
+def main(username, password, sec_id, amount, base_url):
+    login = LoginUsers(username=username,
+                       password=password,
+                       sec_id=sec_id)
+    jwt = login.login_users(base_url)
+    result = post_deposits(jwt_token=jwt,
+                           amount=amount,
+                           base_url=base_url)
+    return result
+
+
+if __name__ == '__main__':
+    main(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID, base_url=BASE_URL_DEV, amount=10000)
