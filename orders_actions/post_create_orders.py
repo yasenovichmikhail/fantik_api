97f73c4716ea0b4d397ad6fd917e9ab9dacb5f2c
user <yasenovich.mikhail@mail.ru>
2024-08-01 13:54:09 +0300

    added duration parameter

--- orders_actions/post_create_orders.py
+++ orders_actions/post_create_orders.py
@@ -3,7 +3,7 @@ from config.config import *
 from user_actions.login_user import LoginUsers
 
 
-def create_orders(jwt_token, action_type, amount, schema, aweme_id=None):
+def create_orders(jwt_token, action_type, amount, duration, base_url, aweme_id=None):
     try:
         headers = {
             'accept': 'application/json',
@@ -15,12 +15,12 @@ def create_orders(jwt_token, action_type, amount, schema, aweme_id=None):
             "awemeId": aweme_id,
             "shortLink": None,
             "actionsAmount": amount,
-            "orderDurationHours": 1,
+            "orderDurationHours": duration,
             "orderName": "test",
             "rewardId": None
         }
 
-        response = requests.post(f'{schema}' + f'{ORDERS_CREATE_PATH}', headers=headers, json=body)
+        response = requests.post(f'{base_url}' + f'{ORDERS_CREATE_PATH}', headers=headers, json=body)
         if response.status_code == 200:
             print(f'Your order successfully created', f'Status code: {response.status_code}', sep='\n')
         else:
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     def main(env):
         login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
         jwt = login.login_users(env=env)
-        create_orders(jwt_token=jwt, action_type=1, amount=10, schema=env)
+        create_orders(jwt_token=jwt, action_type=1, amount=10, base_url=env)
 
     main(BASE_URL_DEV)
 
