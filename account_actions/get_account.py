21f8eb6d16681de71e25d0a7d893311e26fa1ebf
user <yasenovich.mikhail@mail.ru>
2024-08-05 17:21:14 +0300

    modified get_account function. Added base_url parameter

--- account_actions/get_account.py
+++ account_actions/get_account.py
@@ -3,17 +3,28 @@ import requests
 from user_actions.login_user import LoginUsers
 from pprint import *
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt_auth = login.login_users()
+
+def get_account_info(jwt_token, base_url):
+    try:
+        headers = {'Authorization1': jwt_token}
+
+        response = requests.get(f'{base_url}' + f'{GET_ACCOUNT_PATH}', headers=headers)
+        print(f'Status code: {response.status_code}')
+        assert response.status_code == 200, f"Request is failed with status code: {response.status_code}"
+        return response.json()
+    except BaseExceptions:
+        print('Error occurred:\n', traceback.format_exc())
 
 
-def get_account_info():
-    headers = {'Authorization1': jwt_auth}
+def main(username, password, sec_id, base_url):
+    login = LoginUsers(username=username,
+                       password=password,
+                       sec_id=sec_id)
+    jwt = login.login_users(base_url)
+    result = get_account_info(jwt_token=jwt,
+                              base_url=base_url)
+    return pprint(result)
 
-    response = requests.get(f'{BASE_URL}' + f'{GET_ACCOUNT_PATH}', headers=headers)
-    print(f'Status code: {response.status_code}')
-    assert response.status_code == 200, f"Request is failed with status code: {response.status_code}"
-    pprint(response.json())
 
-
-get_account_info()
+if __name__ == '__main__':
+    main(USER_NAME, PASSWORD, SEC_ID, BASE_URL_DEV)
