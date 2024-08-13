654772888026aa456b867bafd561749af21eb2b0
user <yasenovich.mikhail@mail.ru>
2024-08-13 17:24:31 +0300

    added choose base_url parameter

--- orders_actions/get_orders_execution.py
+++ orders_actions/get_orders_execution.py
@@ -4,17 +4,22 @@ from user_actions.login_user import LoginUsers
 from pprint import *
 
 
-def get_orders_execution(jwt_token):
+def get_orders_execution(jwt_token, base_url):
     headers = {
         'accept': 'application/json',
         'Authorization1': jwt_token
     }
 
-    response = requests.get(f'{BASE_URL}' + f'{GET_ORDERS_EXECUTION_PATH}', headers=headers)
+    response = requests.get(f'{base_url}' + f'{GET_ORDERS_EXECUTION_PATH}', headers=headers)
     print(f'Status code: {response.status_code}')
     return pprint(response.json())
 
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt = login.login_users()
-get_orders_execution(jwt)
+def main(base_url):
+    login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
+    jwt = login.login_users(base_url)
+    get_orders_execution(jwt, base_url)
+
+
+if __name__ == '__main__':
+    main(BASE_URL_PROD)
