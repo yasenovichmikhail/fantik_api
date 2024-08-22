d352d35c42512a4cde06f8e7fccc1a6e6bd4dcc8
user <yasenovich.mikhail@mail.ru>
2024-08-22 12:23:21 +0300

    refactoring of the code, added choose base_url parameter

--- user_actions/email_bound.py
+++ user_actions/email_bound.py
@@ -3,39 +3,41 @@ from config.config import *
 from user_actions.login_user import LoginUsers
 
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt_auth = login.login_users()
-
-
-def email_bound_request(jwt_token):
+def email_bound_request(jwt_token, email, base_url):
     headers = {
         'Authorization1': jwt_token
     }
 
     body = {
-        'email': EMAIL
+        'email': email
     }
 
-    response = requests.post(f'{BASE_URL}' + f'{POST_EMAIL_REQUEST_PATH}', headers=headers,
+    response = requests.post(f'{base_url}' + f'{POST_EMAIL_REQUEST_PATH}', headers=headers,
                              json=body)
     key = response.json()['key']
-    # print(f'Status code: {response.status_code}', key, sep='\n')
     return key
 
 
-def email_bound_confirm(key):
+def email_bound_confirm(key, base_url):
     body = {
         'key': key
     }
 
-    response = requests.post(f'{BASE_URL}' + f'{POST_EMAIL_CONFIRM_PATH}', json=body)
+    response = requests.post(f'{base_url}' + f'{POST_EMAIL_CONFIRM_PATH}', json=body)
     print(f'Status code: {response.status_code}', f'Your email has been changed successfully', sep='\n')
 
 
-def bound_email(jwt):
-    key = email_bound_request(jwt)
-    email_bound_confirm(key)
+def bound_email(jwt_token, email, base_url):
+    key = email_bound_request(jwt_token=jwt_token, email=email, base_url=base_url)
+    email_bound_confirm(key=key, base_url=base_url)
 
 
-bound_email(jwt_auth)
+def main(username, password, sec_id, email, base_url):
+    login = LoginUsers(username=username, password=password, sec_id=sec_id)
+    jwt = login.login_users(base_url=base_url)
+    bound_email(jwt_token=jwt, email=email, base_url=base_url)
+
+
+if __name__ == '__main__':
+    main(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID, email=EMAIL, base_url=BASE_URL_DEV)
 
