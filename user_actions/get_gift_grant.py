16406b53f6366d464709508c39dd7c96467ed0c8
user <yasenovich.mikhail@mail.ru>
2024-08-22 12:02:29 +0300

    added choose base_url parameter

--- user_actions/get_gift_grant.py
+++ user_actions/get_gift_grant.py
@@ -4,14 +4,14 @@ from user_actions.login_user import LoginUsers
 from pprint import *
 
 
-def get_gift_grant(jwt_token):
+def get_gift_grant(jwt_token, base_url):
     try:
         headers = {
             'accept': 'application/json',
             'Authorization1': jwt_token
         }
 
-        response = requests.get(f'{BASE_URL}' + f'{GET_GIFT_GRANT_PATH}', headers=headers)
+        response = requests.get(f'{base_url}' + f'{GET_GIFT_GRANT_PATH}', headers=headers)
         if response.status_code == 200:
             print(f'Status code: {response.status_code}', f'The gift was successfully used', sep='\n')
             pprint(response.json())
@@ -19,10 +19,15 @@ def get_gift_grant(jwt_token):
             print(f'Status code: {response.status_code}', f'The gift is not available', sep='\n')
         else:
             print(f'Something went wrong, status code: {response.status_code}')
-    except:
-        BaseException("Error occurred")
+    except BaseExceptions:
+        print('Error occurred:\n', traceback.format_exc())
 
 
-login = LoginUsers(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID)
-jwt = login.login_users()
-get_gift_grant(jwt)
+def main(username, password, sec_id, base_url):
+    login = LoginUsers(username=username, password=password, sec_id=sec_id)
+    jwt = login.login_users(base_url=base_url)
+    get_gift_grant(jwt_token=jwt, base_url=base_url)
+
+
+if __name__ == '__main__':
+    main(username=USER_NAME, password=PASSWORD, sec_id=SEC_ID, base_url=BASE_URL_PROD)
