15828a5a9f5d51bb8b9cf4027e2bd59d2e58b019
user <yasenovich.mikhail@mail.ru>
2024-09-02 14:02:24 +0300

    modified change_user_status script

--- user_actions/change_user_status.py
+++ user_actions/change_user_status.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sqlalchemy import text
 from fetch_sec_id import fetch_sec_id, fetch_user_data
 from config.config import *
 
@@ -9,38 +9,26 @@ def get_sec_id(username):
     return sec_id
 
 
-def get_user_id(sec_id, conn):
-    select_user_id = f"""Select
-    user_id from tm_users
-    where sec_user_id = '{sec_id}'
-    and user_status_id = 1 
-    """
-    result = pd.read_sql(select_user_id, conn)
-    return result.iloc[0]['user_id']
-
-
-def get_user_status(user_id, conn):
-    select_user_status = f"""Select
-    user_status_id from tm_users
-    where user_id = {user_id}
-    and user_status_id = 1
-    """
-    result = pd.read_sql(select_user_status, conn)
-    return result.iloc[0]['user_status_id']
+def delete_user(sec_id, conn):
+    change_user_status = f"""Update
+        tm_users set user_status_id = 2
+        where sec_user_id = '{sec_id}'
+        and user_status_id = 1"""
+    try:
+        with conn.connect() as conn:
+            conn.execute(text(change_user_status))
+            conn.commit()
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
 
 
 def main(username, conn):
     sec_id = get_sec_id(username=username)
-    user_id = get_user_id(sec_id=sec_id,
-                          conn=conn)
-    user_status_id = get_user_status(user_id=user_id,
-                                     conn=conn)
-    return user_status_id
+    delete_user(sec_id=sec_id, conn=conn)
+    print(f"User {username} has been deleted")
 
 
-print(main(username='jimmymacnaxer', conn=DB_PROD_CONNECTION))
-
-# print(get_user_id(get_sec_id('jimmymacnaxer'), DB_PROD_CONNECTION))
+main(username='pes', conn=DB_DEV_CONNECTION)
 
 
 
