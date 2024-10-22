f7279d565450c026d0257d2e8b0b6972727cbc7a
user <yasenovich.mikhail@mail.ru>
2024-10-22 15:45:40 +0300

    modified ban_users script

--- user_actions/ban_user.py
+++ user_actions/ban_user.py
@@ -4,25 +4,22 @@ from user_actions.login_user import LoginUsers
 from pprint import *
 
 
-def ban_user(user_id):
+def ban_user(user_id, base_url):
     try:
-        headers = {
+        params = {
             'user_id': user_id
         }
 
-        response = requests.get(f'{BASE_URL_PROD}' + f'{BAN_USERS}', headers=headers)
+        response = requests.get(f'{base_url}' + f'{BAN_USERS}', params=params)
         if response.status_code == 200:
             print(f'Status code: {response.status_code}', sep='\n')
-            print(response.content)
+            print(f'User {user_id} was banned')
         else:
             print(f'Something went wrong, status code: {response.status_code}')
     except:
         BaseException("Error occurred")
 
 
-def main(user_id):
-    ban_user(user_id=user_id)
-
-
 if __name__ == '__main__':
-    main(user_id=1408103)
+    ban_user(user_id=1460036,
+             base_url=BASE_URL_PROD)
